1ec522a0804494996db32d1c0ee0947d9901779f
Nyogsothep5 <1843739917@qq.com>
2021-09-24 09:17:25 +0800

    finish

---
+++ prac_03/password_check.py
@@ -0,0 +1,29 @@
+"""
+CP1404/CP5632 - Practical
+Password checker "skeleton" code to help you get started
+"""
+
+MIN_LENGTH = 2
+
+
+def main():
+    """Program to get and check a user's password."""
+    print("Please enter a valid password")
+    print("Your password must be longer than", MIN_LENGTH)
+    password = input("> ")
+    while not is_valid_password(password):
+        print("Invalid password!")
+        password = input("> ")
+
+
+def is_valid_password(password):
+    """Determine if the provided password is valid."""
+    if len(password) < MIN_LENGTH:
+        return False
+    else:
+        n = int(len(password))
+        for i in range(n+1):
+            print('*', end="")
+    return True
+
+main()
